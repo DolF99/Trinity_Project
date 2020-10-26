26444a0b0b58333396228a09084e973f38a139ab
DolF99 <DolF99@github.com>
2020-10-26 21:05:20 +0900

    카메라 플레이어 추격

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public Transform target;
+    bool isFindTarget = false;
+    
+    public float p_x = 0f;
+    public float p_y = 200f;
+    public float p_z = 0f;
 
-    Transform cam;
+    Vector3 CameraPos;
 
     void Awake()
     {
-        cam = GetComponent<Transform>();
     }
 
     void Update()
     {
+        if (!isFindTarget)
+        {
+            target = GameObject.Find("Player(Clone)").GetComponent<Transform>();
+            isFindTarget = true;
+        }
+        else
+        {
+            CameraPos.x = target.transform.position.x + p_x;
+            CameraPos.y = target.transform.position.y + p_y;
+            CameraPos.z = target.transform.position.z + p_z;
+
+            transform.position = CameraPos;
+        }
     }
 }
