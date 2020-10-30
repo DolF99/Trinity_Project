dd4c27bff1a2c39412948e870c33c35fd82e03f0
lck5194 <lck5194@naver.com>
2020-10-30 18:01:57 +0900

    tsad

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+
+
+    Sprite Card_image;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Card_image = gameObject.GetComponent<SpriteRenderer>().sprite;
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("ride_SEc");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Script/Main_Scenes_UI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Scenes_UI : MonoBehaviour
+{
+
+    public void onSingleBtn()
+    {
+        Application.LoadLevel("SingleMode");
+    }
+    public void onMultiBtn()
+    {
+        //Application.LoadLevel("");
+        Debug.Log("멀티");
+    }
+    public void onExitBtn()
+    {
+        //Application.LoadLevel("");
+        Debug.Log("종료");
+    }
+
+}
